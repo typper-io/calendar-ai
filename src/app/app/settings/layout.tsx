17459295561013d8047ac8c4042bd56a72d50840
João Barros <augustoj311@gmail.com>
2024-09-14 18:37:10 -0300

    Refactor settings layout component

--- src/app/app/settings/layout.tsx
+++ src/app/app/settings/layout.tsx
@@ -2,9 +2,6 @@ import Link from 'next/link'
 
 import { ReactNode } from 'react'
 
-export const description =
-  'A settings page. The settings page has a sidebar navigation and a main content area. The main content area has a form to update the store name and a form to update the plugins directory. The sidebar navigation has links to general, security, integrations, support, organizations, and advanced settings.'
-
 export default function SettingsLayout({ children }: { children: ReactNode }) {
   return (
     <div className="flex min-h-screen w-full flex-col">
